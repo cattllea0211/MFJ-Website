314f8bf9e231a77b7a7c4ea6fe03be4737e309b5
mao <leamanliguez728@gmail.com>
2025-05-09 03:58:27 +0800

    Update manager_update_service.php

--- manager_update_service.php
+++ manager_update_service.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mfj_db";
+$dbname = "mfjdb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
