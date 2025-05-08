3b7f86d0bd1989f0f7f197896ff1fcc67373e79a
mao <leamanliguez728@gmail.com>
2025-05-09 03:16:58 +0800

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root"; 
 $password = ""; 
-$dbname = "mfj_db"; 
+$dbname = "mfjdb"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
