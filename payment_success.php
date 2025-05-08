44e4a34f4cca23533c79355b4ba121160bdc5295
mao <leamanliguez728@gmail.com>
2025-05-09 04:00:30 +0800

    Update payment_success.php

--- payment_success.php
+++ payment_success.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mfj_db";
+$dbname = "mfjdb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
