abc9ee589441df03f00be28f15fe1f12f02205a3
mao <leamanliguez728@gmail.com>
2025-05-09 13:51:40 +0800

    Update job_assignment.php

--- job_assignment.php
+++ job_assignment.php
@@ -424,7 +424,7 @@ function calculateDaysLeft($scheduledDate) {
 
         <div class="bg-[#1d7691] p-6 flex justify-between items-center rounded-t-3xl shadow-md">
             <div class="flex items-center space-x-4">
-                <a href="employee_dashboard.php" class="text-white hover:bg-indigo-600 p-2 rounded-full transition">
+                 <a href="employee_dashboard.php" class="text-white hover:bg-teal-600 p-2 rounded-full transition">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                     </svg>
